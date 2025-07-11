d1df6661685db873861d700d5afa2afbcbe4ccb7
JamesJMakhlouf <james.makhlouf@lau.edu>
2025-07-11 14:17:00 +0300

    Add Back Background Imags

--- lib/components/dash_background.dart
+++ lib/components/dash_background.dart
@@ -13,17 +13,17 @@ class DashBackground extends ParallaxComponent<FlappyDashGame> with FlameBlocRea
     anchor = Anchor.center;
     parallax = await game.loadParallax(
       [
-        // ParallaxImageData('background/_11_background.png'),
-        // ParallaxImageData('background/_10_distant_clouds.png'),
-        // ParallaxImageData('background/_09_distant_clouds1.png'),
-        // ParallaxImageData('background/_08_clouds.png'),
-        // ParallaxImageData('background/_07_huge_clouds.png'),
-        // ParallaxImageData('background/_06_hill2.png'),
-        // ParallaxImageData('background/_05_hill1.png'),
-        // ParallaxImageData('background/_04_bushes.png'),
-        // ParallaxImageData('background/_03_distant_trees.png'),
-        // ParallaxImageData('background/_02_trees_and_bushes.png'),
-        // ParallaxImageData('background/_01_ground.png'),
+        ParallaxImageData('background/_11_background.png'),
+        ParallaxImageData('background/_10_distant_clouds.png'),
+        ParallaxImageData('background/_09_distant_clouds1.png'),
+        ParallaxImageData('background/_08_clouds.png'),
+        ParallaxImageData('background/_07_huge_clouds.png'),
+        ParallaxImageData('background/_06_hill2.png'),
+        ParallaxImageData('background/_05_hill1.png'),
+        ParallaxImageData('background/_04_bushes.png'),
+        ParallaxImageData('background/_03_distant_trees.png'),
+        ParallaxImageData('background/_02_trees_and_bushes.png'),
+        ParallaxImageData('background/_01_ground.png'),
       ],
       baseVelocity: Vector2(1, 0),
       velocityMultiplierDelta: Vector2(1.5, 0)
